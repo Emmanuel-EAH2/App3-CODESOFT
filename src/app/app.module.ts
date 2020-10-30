c53544f0a92b98ba7997bdee8ee5cb5701b03559
Emmanuel-EAH2 <cetis634@gmail.com>
2020-10-30 11:33:00 -0600

    Se guardaron los productos y estan siendo ejecutados en home.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{ RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    RouterModule.forRoot([
+    {path: 'home', component: HomeComponent},
+    {path: 'About', component: AboutComponent}
+    ])
+],
   providers: [],
   bootstrap: [AppComponent]
 })

2769dcace1d00919f060dfd940f7992680b76542
Emmanuel-EAH2 <cetis634@gmail.com>
2020-11-01 23:30:03 -0600

    se guardarons los componentes en las carpdtas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import{ RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { CamarasComponent } from './camaras/camaras.component';
-import { CelularesComponent } from './celulares/celulares.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './Shared/about/about.component';
+import { CamarasComponent } from './components/camaras/camaras.component';
+import { CelularesComponent } from './components/celulares/celulares.component';
 
 @NgModule({
   declarations: [
